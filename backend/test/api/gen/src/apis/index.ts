566c94f3715a17845f67825a95bac218c8251997
wsdt <kevin.riedl@wavect.io>
2024-08-12 12:13:28 +0200

    feat: OpenAPI gen for backend

---
+++ backend/test/api/gen/src/apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './DefaultApi';
+export * from './PushNotificationsApi';
+export * from './UserApi';
+export * from './UserReportsApi';
---
+++ backend/test/api/gen/src/models/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BlacklistedRegion';
+export * from './BlacklistedRegionDTO';
+export * from './CreateUserDTO';
+export * from './CreateUserReportDto';
+export * from './StorePushTokenDTO';
+export * from './UpdateUserDTO';
+export * from './User';
+export * from './UserPublicDTO';
+export * from './UserReport';
