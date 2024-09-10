3d1576914215ba8765db0cc99c22d3a20a491956
wsdt <kevin.riedl@wavect.io>
2024-09-10 13:10:54 +0200

    fix: Location perm & task should be started only when enabling toggle

--- mobile/components/OGoLiveToggle/OGoLiveToggle.tsx
+++ mobile/components/OGoLiveToggle/OGoLiveToggle.tsx
@@ -1,5 +1,6 @@
 import { Color } from "@/GlobalStyles";
 import {
+    LocationUpdateDTO,
     UpdateUserDTO,
     UserApi,
     UserApproachChoiceEnum,
@@ -7,16 +8,101 @@ import {
 } from "@/api/gen/src";
 import { EACTION_USER, useUserContext } from "@/context/UserContext";
 import { TR, i18n } from "@/localization/translate.service";
+import {
+    SECURE_VALUE,
+    getSecurelyStoredValue,
+} from "@/services/secure-storage.service";
+import { getLocallyStoredUserData } from "@/services/storage.service";
 import { includeJWT } from "@/utils/misc.utils";
 import * as Location from "expo-location";
+import * as TaskManager from "expo-task-manager";
 import { StyleProp, Switch, Text, View, ViewStyle } from "react-native";
 
 interface IOGoLiveToggleProps {
     style?: StyleProp<ViewStyle>;
 }
 
+const LOCATION_TASK_NAME = "background-location-task";
+
+// Define the background task for location tracking
+TaskManager.defineTask(LOCATION_TASK_NAME, async ({ data, error }) => {
+    if (error) {
+        console.error(`Task error ${error}`);
+        return;
+    }
+    if (data) {
+        const locations = (data as any).locations as Location.LocationObject[];
+        const user = getLocallyStoredUserData();
+        console.log("User Connected: ", user?.id?.slice(0, 8));
+        const userId = user?.id;
+        const jwtToken = getSecurelyStoredValue(SECURE_VALUE.JWT_ACCESS_TOKEN);
+        if (!userId || !jwtToken) {
+            console.error(
+                "UserID and/or jwtToken undefined in location task service.",
+            );
+            return;
+        }
+        const userApi = new UserApi();
+
+        if (locations && locations.length > 0 && userId) {
+            const location = locations[locations.length - 1];
+            const locationUpdateDTO: LocationUpdateDTO = {
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+            };
+
+            try {
+                await userApi.userControllerUpdateLocation(
+                    {
+                        userId,
+                        locationUpdateDTO: locationUpdateDTO,
+                    },
+                    await includeJWT(),
+                );
+                console.log(
+                    `[TASK:LOCATION_UPDATE]: User Location updated successfully`,
+                );
+            } catch (error) {
+                console.error(
+                    "Error updating location:",
+                    JSON.stringify(error),
+                );
+            }
+        }
+    }
+});
+
 export const OGoLiveToggle = (props: IOGoLiveToggleProps) => {
     const { dispatch, state } = useUserContext();
+
+    const configureLocationTracking = async (newDateMode: UserDateModeEnum) => {
+        if (newDateMode === UserDateModeEnum.live) {
+            const { status } =
+                await Location.requestBackgroundPermissionsAsync();
+            if (status === "granted" && state.id) {
+                console.log(`Live mode: Starting task ${LOCATION_TASK_NAME}`);
+                await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+                    accuracy: Location.Accuracy.BestForNavigation,
+                    timeInterval: 5000,
+                    distanceInterval: 10,
+                    foregroundService: {
+                        notificationTitle: "Background location use",
+                        notificationBody:
+                            "Tracking your location in the background.",
+                    },
+                });
+            }
+        } else {
+            try {
+                await Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME);
+            } catch (err) {
+                console.error(err);
+            }
+            console.log(
+                "Not running location service due to user settings (ghost mode).",
+            );
+        }
+    };
 
     const toggleSwitch = async () => {
         try {
@@ -57,6 +143,8 @@ export const OGoLiveToggle = (props: IOGoLiveToggleProps) => {
                     dateMode: newDateMode,
                 },
             });
+
+            await configureLocationTracking(newDateMode);
 
             // Load location and inform user about state
             if (newDateMode === UserDateModeEnum.live) {
