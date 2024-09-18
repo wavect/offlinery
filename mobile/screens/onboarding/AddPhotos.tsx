36697d7fa078a440a3d3f50c572ee65fb602d952
wsdt <kevin.riedl@wavect.io>
2024-09-18 18:39:59 +0200

    chore: move photo container into sep component

--- mobile/screens/onboarding/AddPhotos.tsx
+++ mobile/screens/onboarding/AddPhotos.tsx
@@ -1,105 +1,18 @@
-import { BorderRadius, Color } from "@/GlobalStyles";
 import { MainStackParamList } from "@/MainStack.navigator";
 import { UserApi } from "@/api/gen/src";
 import { OButtonWide } from "@/components/OButtonWide/OButtonWide";
 import { OPageContainer } from "@/components/OPageContainer/OPageContainer";
-import {
-    EACTION_USER,
-    IUserAction,
-    IUserData,
-    ImageIdx,
-    isImagePicker,
-    useUserContext,
-} from "@/context/UserContext";
+import { PhotoContainer } from "@/components/OPhotoContainer/OPhotoContainer";
+import { ImageIdx, isImagePicker, useUserContext } from "@/context/UserContext";
 import { TR, i18n } from "@/localization/translate.service";
 import { includeJWT } from "@/utils/misc.utils";
-import { MaterialIcons } from "@expo/vector-icons";
 import * as ImagePicker from "expo-image-picker";
 import { ImagePickerAsset } from "expo-image-picker";
 import * as React from "react";
 import { useState } from "react";
-import {
-    Image,
-    Pressable,
-    StyleSheet,
-    View,
-    useWindowDimensions,
-} from "react-native";
+import { StyleSheet, View, useWindowDimensions } from "react-native";
 import { NativeStackScreenProps } from "react-native-screens/native-stack";
 import { ROUTES } from "../routes";
-
-interface IPhotoContainerProps {
-    imageIdx: ImageIdx;
-    dispatch: React.Dispatch<IUserAction>;
-    state: IUserData;
-    size: number;
-    mediaStatus: ImagePicker.MediaLibraryPermissionResponse | null;
-    requestMediaLibPermission: () => Promise<ImagePicker.MediaLibraryPermissionResponse>;
-}
-
-const PhotoContainer = (props: IPhotoContainerProps) => {
-    const {
-        dispatch,
-        imageIdx,
-        state,
-        mediaStatus,
-        requestMediaLibPermission,
-        size,
-    } = props;
-
-    const openMediaLibrary = async () => {
-        let mediaAccessGranted = mediaStatus?.granted;
-        if (!mediaAccessGranted) {
-            mediaAccessGranted = (await requestMediaLibPermission()).granted;
-        }
-        if (mediaAccessGranted) {
-            let result = await ImagePicker.launchImageLibraryAsync({
-                allowsEditing: true,
-                quality: 1,
-                allowsMultipleSelection: false,
-            });
-
-            if (!result.canceled) {
-                // remove, override, add image
-                const image = result.assets[0];
-                dispatch({
-                    type: EACTION_USER.UPDATE_MULTIPLE,
-                    payload: {
-                        imageURIs: {
-                            ...state.imageURIs,
-                            [imageIdx]: { ...image, fileName: imageIdx },
-                        },
-                    },
-                });
-            }
-        } else {
-            alert(i18n.t(TR.noAccessToMediaLib));
-        }
-    };
-
-    const img = state.imageURIs[imageIdx];
-
-    // If the image is an `ImagePicker` we can directly access image on the user's device
-    // otherwise we need to fetch it from the server.
-    const uri = isImagePicker(img) ? img.uri : img;
-
-    return (
-        <Pressable
-            style={[styles.photoContainer, { width: size, height: size }]}
-            onPress={openMediaLibrary}
-        >
-            {!img ? (
-                <MaterialIcons
-                    name="add-circle-outline"
-                    size={size * 0.2}
-                    color={Color.primary}
-                />
-            ) : (
-                <Image style={styles.previewImage} source={{ uri }} />
-            )}
-        </Pressable>
-    );
-};
 
 const userApi = new UserApi();
 const AddPhotos = ({
@@ -209,21 +122,6 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         flexWrap: "wrap",
         justifyContent: "space-between",
-    },
-    previewImage: {
-        width: "100%",
-        height: "100%",
-        borderRadius: BorderRadius.br_5xs,
-    },
-    photoContainer: {
-        marginBottom: 5,
-        borderWidth: 1,
-        borderRadius: BorderRadius.br_5xs,
-        borderColor: Color.primary,
-        borderStyle: "dashed",
-        backgroundColor: Color.brightGray,
-        justifyContent: "center",
-        alignItems: "center",
     },
     row: {
         flexDirection: "row",
