2eca3cca6dc8783b710b3d64a8fb4c9c9f73d113
jocr <christof.jori.dev@gmail.com>
2024-09-20 17:11:21 +0200

    fix: runtime

--- mobile/api/gen/src/runtime.ts
+++ mobile/api/gen/src/runtime.ts
@@ -1,5 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
+import Constants from "expo-constants";
+import { isExpoGoEnvironment } from "../../../utils/misc.utils";
 
 /**
  * Offlinery
@@ -15,7 +17,11 @@
 
 // export const BASE_PATH = "http://localhost:3000/v1".replace(/\/+$/, "");
 
-export const BASE_PATH = "http://localhost:3000/v1";
+export const BASE_PATH = (
+    (isExpoGoEnvironment
+        ? `http://${Constants.expoConfig?.hostUri?.split(":").shift()?.concat(":3000")}`
+        : "https://offlinery.onrender.com") + "/v1"
+).replace(/\/+$/, "");
 
 export interface ConfigurationParameters {
     basePath?: string; // override base path
