879396ed65634b1df248439d568ddcfb3103b952
wsdt <kevin.riedl.privat@gmail.com>
2024-09-27 21:47:04 +0200

    feat: Add remove img, OF-184

--- mobile/components/OPhotoContainer/OPhotoContainer.tsx
+++ mobile/components/OPhotoContainer/OPhotoContainer.tsx
@@ -10,7 +10,7 @@ import { getValidImgURI } from "@/utils/media.utils";
 import { MaterialIcons } from "@expo/vector-icons";
 import * as ImagePicker from "expo-image-picker";
 import * as React from "react";
-import { Image, Pressable, StyleSheet } from "react-native";
+import { Image, Pressable, StyleSheet, View } from "react-native";
 
 interface IPhotoContainerProps {
     imageIdx: ImageIdx;
@@ -61,30 +61,64 @@ export const PhotoContainer = (props: IPhotoContainerProps) => {
         }
     };
 
+    const removeImage = () => {
+        dispatch({
+            type: EACTION_USER.UPDATE_MULTIPLE,
+            payload: {
+                imageURIs: {
+                    ...state.imageURIs,
+                    [imageIdx]: null,
+                },
+            },
+        });
+    };
+
     const img = state.imageURIs[imageIdx];
 
     return (
-        <Pressable
-            style={[styles.photoContainer, { width: size, height: size }]}
-            onPress={openMediaLibrary}
-        >
-            {!img ? (
-                <MaterialIcons
-                    name="add-circle-outline"
-                    size={size * 0.2}
-                    color={Color.primary}
-                />
-            ) : (
-                <Image
-                    style={styles.previewImage}
-                    source={{ uri: getValidImgURI(img) }}
-                />
+        <View style={[styles.photoContainer, { width: size, height: size }]}>
+            <Pressable style={styles.photoContent} onPress={openMediaLibrary}>
+                {!img ? (
+                    <MaterialIcons
+                        name="add-circle-outline"
+                        size={size * 0.2}
+                        color={Color.primary}
+                    />
+                ) : (
+                    <Image
+                        style={styles.previewImage}
+                        source={{ uri: getValidImgURI(img) }}
+                    />
+                )}
+            </Pressable>
+            {img && (
+                <Pressable style={styles.removeButton} onPress={removeImage}>
+                    <MaterialIcons
+                        name="close"
+                        size={size * 0.15}
+                        color={Color.white}
+                    />
+                </Pressable>
             )}
-        </Pressable>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
+    photoContent: {
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    removeButton: {
+        position: "absolute",
+        top: 5,
+        right: 5,
+        backgroundColor: Color.redDark,
+        borderRadius: 15,
+        padding: 2,
+    },
     photoContainer: {
         marginBottom: 5,
         borderWidth: 1,
