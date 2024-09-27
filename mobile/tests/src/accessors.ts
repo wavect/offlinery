405250decb95a19965ea1d467ea15bce2e56f3fc
jocr <christof.jori.dev@gmail.com>
2024-09-27 10:38:04 +0200

    test: e2e test: main flow, settings flow

--- mobile/tests/src/accessors.ts
+++ mobile/tests/src/accessors.ts
@@ -14,8 +14,20 @@ export const TestData = {
         buttonSafe: "button-save",
         buttonDangerLogout: "button-logout",
         buttonDangerDeleteAccount: "button-danger-delete-account",
+        changePassword: {
+            oldPw: "old-password",
+            newPw: "new-password",
+            newPwRpt: "new-password-repeat",
+            confirm: "pwd-change-confirm",
+        },
     },
     encounters: {
         inputStatus: "input-update-status",
+        inputEncounterSendMessage: "input-send-encounter-message",
+        buttonEncounterSendMessage: "button-encounter-send-message",
+        heatMapComponent: "heat-map-component",
+    },
+    main: {
+        goLiveBtn: "go-live-button",
     },
 };
