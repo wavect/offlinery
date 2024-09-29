47e39d5244202485d8505dc310f157b50a3ed6d6
wsdt <kevin.riedl.privat@gmail.com>
2024-09-29 11:40:38 +0200

    fix: splash screen

--- mobile/screens/main/FindPeople.tsx
+++ mobile/screens/main/FindPeople.tsx
@@ -1,4 +1,4 @@
-import { OMap, OMapRefType } from "@/components/OMap/OMap";
+import { OMapRefType } from "@/components/OMap/OMap";
 import { OPageContainer } from "@/components/OPageContainer/OPageContainer";
 import { TR, i18n } from "@/localization/translate.service";
 import { MainScreenTabsParamList } from "@/screens/main/MainScreenTabs.navigator";
@@ -6,6 +6,7 @@ import { ROUTES } from "@/screens/routes";
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import * as React from "react";
 import { useRef } from "react";
+import { Text } from "react-native";
 
 const FindPeople = (
     props: BottomTabScreenProps<
@@ -20,12 +21,7 @@ const FindPeople = (
             subtitle={i18n.t(TR.beNearTheseHotspotsToMeet)}
             refreshFunc={oMapRef.current?.getOtherUsersPositions}
         >
-            <OMap
-                ref={oMapRef}
-                saveChangesToBackend={true}
-                showHeatmap={true}
-                showBlacklistedRegions={true}
-            />
+            <Text>test</Text>
         </OPageContainer>
     );
 };
