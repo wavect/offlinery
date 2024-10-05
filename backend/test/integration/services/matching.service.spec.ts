35c4ea2d387086d164808b1a08c2dac623d1f0c1
jocr <christof.jori.dev@gmail.com>
2024-10-05 13:56:28 +0200

    fix: messages

--- backend/test/integration/services/matching.service.spec.ts
+++ backend/test/integration/services/matching.service.spec.ts
@@ -1,46 +1,13 @@
-import { EncounterService } from "@/entities/encounter/encounter.service";
 import { User } from "@/entities/user/user.entity";
-import { UserRepository } from "@/entities/user/user.repository";
 import { MatchingService } from "@/transient-services/matching/matching.service";
-import { NotificationService } from "@/transient-services/notification/notification.service";
 import { EDateMode } from "@/types/user.types";
-import { Test, TestingModule } from "@nestjs/testing";
-import { I18nService } from "nestjs-i18n";
+import { getIntegrationTestModule } from "../../_src/modules/integration-test.module";
 
 describe("MatchingService Integration", () => {
     let matchingService: MatchingService;
 
     beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            providers: [
-                MatchingService,
-                {
-                    provide: UserRepository,
-                    useValue: {
-                        getPotentialMatchesForNotifications: jest.fn(),
-                        getPotentialMatchesForHeatMap: jest.fn(),
-                    },
-                },
-                {
-                    provide: NotificationService,
-                    useValue: {
-                        sendPushNotification: jest.fn(),
-                    },
-                },
-                {
-                    provide: EncounterService,
-                    useValue: {
-                        saveEncountersForUser: jest.fn(),
-                    },
-                },
-                {
-                    provide: I18nService,
-                    useValue: {
-                        t: jest.fn(),
-                    },
-                },
-            ],
-        }).compile();
+        const { module } = await getIntegrationTestModule();
 
         matchingService = module.get<MatchingService>(MatchingService);
     });
@@ -49,10 +16,8 @@ describe("MatchingService Integration", () => {
         it("should return an empty array when user is not sharing location", async () => {
             const user = new User();
             user.dateMode = EDateMode.GHOST;
-
             const result =
                 await matchingService.findPotentialMatchesForHeatmap(user);
-
             expect(result).toEqual([]);
         });
     });
