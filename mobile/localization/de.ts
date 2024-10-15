89c5c4cce95dff2204693fe19cc6b04f1535652f
Dave <47706547+DaveFz@users.noreply.github.com>
2024-10-15 13:54:29 +0200

    Update mobile/localization/de.ts
    Co-authored-by: Riedl Kevin, Bsc. <kevin.riedl@wavect.io>

--- mobile/localization/de.ts
+++ mobile/localization/de.ts
@@ -180,7 +180,7 @@ export const de: Language = {
     calculating: "Berechne..",
     changePreferencesDescr:
         "Ändere deine Einstellungen oder aktualisiere dein Profil.",
-    nonNegotiable: "Nicht Verhandelbar", // TODO: Nicht Verhandelbar sounds weird?
+    nonNegotiable: "Must Have's"
     bio: "Bio",
     defaultBio: "Bitte keine Pick-Up Lines. Ein nettes Hallo reicht völlig.",
     bioTooltip: "Dies wird anderen Nutzern beim Ansprechen angezeigt.",
