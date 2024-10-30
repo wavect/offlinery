3cbf873e3c443d65ab604e2c9193e44b08e2521b
wsdt <kevin.riedl.privat@gmail.com>
2024-10-30 12:57:20 +0100

    fix: datasourcer

--- backend/src/datasource.ts
+++ backend/src/datasource.ts
@@ -1,21 +1,15 @@
-const { TYPED_ENV } = require("./utils/env.utils");
-
 const { DataSource } = require("typeorm");
-
-// Import utilities using relative paths instead of aliases
-const { validateEnv } = require("./utils/env.utils");
-const { IS_DEV_MODE } = require("./utils/misc.utils");
-
-validateEnv();
+const dotenv = require("dotenv");
+dotenv.config();
 
 const dataSource = new DataSource({
     type: "postgres",
-    host: TYPED_ENV.DB_HOST,
-    port: TYPED_ENV.DB_PORT,
-    username: TYPED_ENV.DB_USER,
-    password: TYPED_ENV.DB_PASSWORD,
-    database: TYPED_ENV.DB_DATABASE,
-    synchronize: IS_DEV_MODE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    synchronize: false,
     autoLoadEntities: true,
     migrationsRun: true,
     migrations: [__dirname + "/migrations/**/*{.ts,.js}"],
