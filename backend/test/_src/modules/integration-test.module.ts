5327c7e58ad4daca45d85acfb2cef75645b61513
wsdt <kevin.riedl@wavect.io>
2025-01-09 13:52:25 +0100

    test module

--- backend/test/_src/modules/integration-test.module.ts
+++ backend/test/_src/modules/integration-test.module.ts
@@ -13,6 +13,7 @@ import { UserReportModule } from "@/entities/user-report/user-report.module";
 import { User } from "@/entities/user/user.entity";
 import { UserModule } from "@/entities/user/user.module";
 import { UserRepository } from "@/entities/user/user.repository";
+import { ClusteringModule } from "@/transient-services/clustering/clustering.module";
 import { MatchingModule } from "@/transient-services/matching/matching.module";
 import { TYPED_ENV } from "@/utils/env.utils";
 import { MailerModule } from "@nestjs-modules/mailer";
@@ -72,6 +73,7 @@ export const getIntegrationTestModule = async (): Promise<TestModuleSetup> => {
             MockAuthModule,
             UserReportModule,
             MapModule,
+            ClusteringModule,
             MockMatchingModule,
             UserFeedbackModule,
             PendingUserModule,
