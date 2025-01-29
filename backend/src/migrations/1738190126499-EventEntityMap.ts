78ffb658be167502a37284d3b76a1cb23844a17d
wsdt <kevin.riedl@wavect.io>
2025-01-29 23:40:59 +0100

    fix: migration

--- backend/src/migrations/1738190126499-EventEntityMap.ts
+++ backend/src/migrations/1738190126499-EventEntityMap.ts
@@ -5,19 +5,19 @@ export class EventEntityMap1738190126499 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `DROP INDEX "public"."IDX_643c345f3e874006834b68ef60"`,
+            `ALTER TABLE "translatable_entity" ADD "location" geography(Point,4326)`,
         );
         await queryRunner.query(
-            `ALTER TABLE "translatable_entity" DROP COLUMN "location"`,
+            `CREATE INDEX "IDX_643c345f3e874006834b68ef60" ON "translatable_entity" USING GiST ("location") `,
         );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `ALTER TABLE "translatable_entity" ADD "location" geography(Point,4326)`,
+            `DROP INDEX "public"."IDX_643c345f3e874006834b68ef60"`,
         );
         await queryRunner.query(
-            `CREATE INDEX "IDX_643c345f3e874006834b68ef60" ON "translatable_entity" USING GiST ("location") `,
+            `ALTER TABLE "translatable_entity" DROP COLUMN "location"`,
         );
     }
 }
