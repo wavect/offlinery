d31418c40c8fade721ac593423c9aec3866c7db0
wsdt <kevin.riedl@wavect.io>
2025-01-29 23:36:09 +0100

    chore: migration

---
+++ backend/src/migrations/1738190126499-EventEntityMap.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class EventEntityMap1738190126499 implements MigrationInterface {
+    name = "EventEntityMap1738190126499";
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `DROP INDEX "public"."IDX_643c345f3e874006834b68ef60"`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "translatable_entity" DROP COLUMN "location"`,
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `ALTER TABLE "translatable_entity" ADD "location" geography(Point,4326)`,
+        );
+        await queryRunner.query(
+            `CREATE INDEX "IDX_643c345f3e874006834b68ef60" ON "translatable_entity" USING GiST ("location") `,
+        );
+    }
+}
