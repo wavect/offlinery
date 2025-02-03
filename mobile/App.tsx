469afeeca31930998693112b21c0c00d0e781049
wsdt <kevin.riedl.privat@gmail.com>
2025-02-03 10:30:04 +0100

    fix: story: toolbox

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,8 +2,7 @@ import * as SplashScreen from "expo-splash-screen";
 import StorybookUIRoot from "./.storybook";
 import AppRoot from "./AppRoot";
 
-const isStorybookRun =
-    process.env.EXPO_PUBLIC_STORYBOOK?.toLowerCase() === "true";
+const isStorybookRun = true;
 if (isStorybookRun) {
     // @dev Hide splashscreen as nothing externally to load for storybook
     SplashScreen.hideAsync();
