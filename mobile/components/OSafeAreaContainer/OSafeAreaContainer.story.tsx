ec96dea7c126e08f133d92cd4f3f1013406b2598
wsdt <kevin.riedl.privat@gmail.com>
2025-02-04 15:25:54 +0100

    fix: story:   SafeAreaContainer

--- mobile/components/OSafeAreaContainer/OSafeAreaContainer.story.tsx
+++ mobile/components/OSafeAreaContainer/OSafeAreaContainer.story.tsx
@@ -14,7 +14,7 @@ const meta: Meta<typeof OSafeAreaContainer> = {
     },
     decorators: [
         (Story) => (
-            <div
+            <View
                 style={{
                     width: "100%",
                     height: "100%",
@@ -22,7 +22,7 @@ const meta: Meta<typeof OSafeAreaContainer> = {
                 }}
             >
                 <Story />
-            </div>
+            </View>
         ),
     ],
 };
