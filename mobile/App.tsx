f939115c03511a331f1516ca19d3bfe404951ec4
wsdt <kevin.riedl@wavect.io>
2025-02-05 14:38:14 +0100

    fix: make mergable

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,7 +2,8 @@ import * as SplashScreen from "expo-splash-screen";
 import StorybookUIRoot from "./.storybook";
 import AppRoot from "./AppRoot";
 
-const isStorybookRun = true;
+const isStorybookRun =
+    process.env.EXPO_PUBLIC_STORYBOOK?.toLowerCase() === "true";
 if (isStorybookRun) {
     // @dev Hide splashscreen as nothing externally to load for storybook
     SplashScreen.hideAsync();
